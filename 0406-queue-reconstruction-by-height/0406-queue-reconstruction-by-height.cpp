4ed85c001c908618d44eb21f7cfe6a3e55c6d77e
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-11 21:59:15 -0400

    Time: 37 ms (25.91%), Space: 15.9 MB (34.39%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        // [7, 0], [7, 1], [6, 1], [5, 0], [5, 1], [4, 4]
+        ranges::sort(people, [](const vector<int>& a, const vector<int>& b) {
+            return a[0] > b[0] || (a[0] == b[0] && a[1] < b[1]);
+        });
+        
+        vector<vector<int>> result;
+        for (auto p : people) {
+            result.insert(result.begin() + p[1], p);
+        }
+        
+        return result;
+    }
+};
