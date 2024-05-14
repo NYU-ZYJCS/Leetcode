0c74704a92169fe439c5dd6afcf25e89bd2bcf17
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-13 22:17:49 -0400

    Time: 218 ms (43.68%), Space: 10 MB (36.97%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -6,8 +6,7 @@ public:
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] != 0) {
-                    vector<vector<int>> visited(m, vector<int>(n));
-                    result = max(result, dfs(grid, visited, i, j));
+                    result = max(result, dfs(grid, i, j));
                 }
             }
         }
@@ -15,15 +14,18 @@ public:
         
     }
     
-    int dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, int x, int y) {
-        if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y] == 1 || grid[x][y] == 0) return 0;
+    int dfs(vector<vector<int>>& grid, int x, int y) {
+        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] == 0) return 0;
         
-        visited[x][y] = 1;
-        int left = dfs(grid, visited, x, y - 1);
-        int right = dfs(grid, visited, x, y + 1);
-        int up = dfs(grid, visited, x - 1, y);
-        int down = dfs(grid, visited, x + 1, y);
-        visited[x][y] = 0;
+        int rec = grid[x][y];
+        
+        grid[x][y] = 0;
+        int left = dfs(grid, x, y - 1);
+        int right = dfs(grid, x, y + 1);
+        int up = dfs(grid, x - 1, y);
+        int down = dfs(grid, x + 1, y);
+        grid[x][y] = rec;
+        
         
         return max({left, right, up, down}) + grid[x][y];
     }
