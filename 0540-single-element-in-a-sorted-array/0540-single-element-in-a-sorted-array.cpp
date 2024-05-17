a2f70c3b23f079d67e29bf3be1ac14ee785c9628
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-16 23:12:13 -0400

    Time: 22 ms (31.53%), Space: 24.6 MB (50.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,13 +6,8 @@ public:
         
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (mid & 1) {
-                if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) left = mid + 1;
-                else right = mid;
-            } else {
-                if (mid + 1 < n && nums[mid] == nums[mid + 1]) left = mid + 2;
-                else right = mid;
-            }
+            if (nums[mid] == nums[mid ^ 1]) left = mid + 1;
+            else right = mid;
         }
         
         return nums[left];
