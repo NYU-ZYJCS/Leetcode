7f1747507fc458bb5e396ad28ea52fe3f658ffc1
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-19 16:05:21 -0400

    Time: 21 ms (99.56%), Space: 19.1 MB (83.87%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -4,30 +4,45 @@ public:
         unordered_set<string> dict(wordList.begin(), wordList.end());
         if (dict.find(endWord) == dict.end()) return 0;
         
-        queue<string> q;
-        q.push(beginWord);
+        queue<string> q_start, q_end;
+        unordered_set<string> dict_start, dict_end;
+        
+        q_start.push(beginWord);
+        q_end.push(endWord);
+        dict_start.insert(beginWord);
+        dict_end.insert(endWord);
+        dict.erase(endWord);
+        
         int steps = 1;
         
-        while (!q.empty()) {
-            int size = q.size();
+        
+        while (!q_start.empty() && !q_end.empty()) {
+            if (q_start.size() > q_end.size()) {
+                swap(q_start, q_end);
+                swap(dict_start, dict_end);
+            }
+            
+            int size = q_start.size();
             
             while (size--) {
-                auto curWord = q.front();
-                q.pop();
+                auto curWord = q_start.front();
+                q_start.pop();
 
-                if (curWord == endWord) return steps;
+                // if (dict_end.find(curWord) != dict_end.end()) return steps + 1;
                 
                 for (int i = 0; i < curWord.size(); ++i) {
                     char old = curWord[i];
                     for (char c = 'a'; c <= 'z'; ++c) {
                         if (old == c) continue;
                         curWord[i] = c;
+                        if (dict_end.find(curWord) != dict_end.end()) return steps + 1;
                         if (dict.find(curWord) != dict.end()) {
-                            q.push(curWord);
+                            q_start.push(curWord);
+                            dict_start.insert(curWord);
                             dict.erase(curWord);
                         }
-                        curWord[i] = old;
                     }
+                    curWord[i] = old;
                 }
             }
             ++steps;
