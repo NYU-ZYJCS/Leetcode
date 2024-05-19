82a292a62878bb462f016e78b167a572c3cb683c
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-18 21:49:58 -0400

    Time: 60 ms (36.44%), Space: 28.9 MB (28.40%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,32 +2,49 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1) return -1;
+        
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        if (n == 1 && grid[0][0] == 0) return 1;
         vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
         
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 2;
+        queue<pair<int, int>> q_start, q_end;
+        unordered_set<int> seen_start, seen_end;
+        
+        auto hash = [&](int x, int y) { return n * x + y; };
+        
+        q_start.push({0, 0}), q_end.push({n - 1, n - 1});
+        seen_start.insert(hash(0, 0)), seen_end.insert(hash(n - 1, n - 1));
         int steps = 1;
         
-        while (!q.empty()) {
-            int size = q.size();
+        while (!q_start.empty() && !q_end.empty()) {
+            if (q_start.size() > q_end.size()) {
+                swap(q_start, q_end);
+                swap(seen_start, seen_end);
+            }
             
+            int size = q_start.size();
             while (size--) {
-                auto [x, y] = q.front();
-                q.pop();
-                if (x == n - 1 && y == n - 1) return steps;
-
+                auto [x, y] = q_start.front();
+                q_start.pop();
+                
                 for (auto dir : directions) {
                     int nx = x + dir.first;
                     int ny = y + dir.second;
+
                     if (nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny] == 0) {
-                        grid[nx][ny] = 2;
-                        q.push({nx, ny});
+                        int hashCode = hash(nx, ny);
+
+                        if (seen_end.count(hashCode)) {
+                            return steps + 1;
+                        }
+
+                        if (!seen_start.count(hashCode)) {
+                            seen_start.insert(hashCode);
+                            q_start.push({nx, ny});
+                        }
                     }
                 }
             }
-            
             steps++;
         }
         
