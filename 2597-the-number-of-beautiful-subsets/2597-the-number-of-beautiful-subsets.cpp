80a8686d48524888044547f596f387c54a4d72c0
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-22 22:52:18 -0400

    Time: 7 ms (97.73%), Space: 38.2 MB (50.62%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
-        int result = -1;
-        int n = nums.size();
-        int cnt[3001]{};
+        unordered_map<int, map<int, int>> groups;
         
-        function<void(int)> helper = [&] (int pos) {
-            ++result;
-            if (pos == n) return;
+        for (int x : nums) groups[x % k][x]++;
+        
+        int res = 1;
+        for (auto& [_, g] : groups) {
+            int n = g.size();
+            int dp[n + 1];
+            auto it = g.begin();
+            dp[0] = 1;
+            dp[1] = 1 << it++->second;
             
-            for (int i = pos; i < n; ++i) {
-                int x = nums[i] + k;
-                if (cnt[x - k] == 0 && cnt[x + k] == 0) {
-                    ++cnt[x];
-                    helper(i + 1);
-                    --cnt[x];
+            for (int i = 2; it != g.end(); ++it, ++i) {
+                if (it->first - prev(it)->first == k) {
+                    dp[i] = dp[i - 1] + dp[i - 2] * ((1 << it->second) - 1);
+                } else {
+                    dp[i] = dp[i - 1]  << it->second;
                 }
             }
-        };
+            
+            res *= dp[n];
+        }
         
-        helper(0);
-        return result;
+        return res - 1;
     }
 };
