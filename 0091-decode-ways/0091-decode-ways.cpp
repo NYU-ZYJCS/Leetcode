bc6884cbd05f0d1b535a58ed3e56ed702ec1db74
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-24 17:00:58 -0400

    Time: 0 ms (100.00%), Space: 8.8 MB (31.18%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -4,16 +4,15 @@ public:
         int n = s.size();
         vector<int> dp(n + 1, 0);
         if (s[0] == '0') return 0;
-
+        
         dp[0] = 1;
         dp[1] = 1;
         for (int i = 2; i <= n; ++i) {
-            if (s[i - 1] != '0') dp[i] += dp[i - 1];
+            if (s[i - 1] != '0') dp[i] = dp[i - 1];
             
-            string str = s.substr(i - 2, 2);
-            if (str >= "10" && str <= "26") dp[i] += dp[i - 2];
+            string temp = s.substr(i - 2, 2);
+            if (stoi(temp) >= 10 && stoi(temp) <= 26) dp[i] += dp[i - 2];
         }
-
         return dp.back();
     }
 };
