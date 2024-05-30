e21cdf58e58691d93f8be744ada1406865959833
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-29 22:39:23 -0700

    Time: 63 ms (92.89%), Space: 78.3 MB (93.58%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int n = pref.size();
+        vector<int> res(n);
+        
+        res[0] = pref[0];
+        for (int i = 1; i < n; ++i) {
+            res[i] = pref[i - 1] ^ pref[i];
+        }
+        
+        // prefix[i] = prefix[i - 1] ^ array[i]
+        // prefix[i] & prefix[i - 1] = array[i]
+        
+        return res;
+    }
+};
