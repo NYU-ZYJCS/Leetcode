89c7de3960cfa651e39d1095b3062ec29511626d
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-05-29 23:27:43 -0700

    Time: 0 ms (100.00%), Space: 10.1 MB (10.47%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int countTriplets(vector<int>& arr) {
         int n = arr.size();
-        int res = 0;
-        vector<int> prefix(n + 1);
+        vector<int> s(n + 1);
+        
         for (int i = 0; i < n; ++i) {
-            prefix[i + 1] = prefix[i] ^ arr[i];
+            s[i + 1] = s[i] ^ arr[i];
         }
         
-        // i..j-1  =>  prefix[]
-        // j..k  =>  
-        for (int i = 0; i < n; ++i) {
-            for (int k = i + 1; k < n; ++k) {
-                if (prefix[k + 1] == prefix[i]) {
-                    res += k - i;
-                }
+        unordered_map<int, int> cnt, total;
+        int res = 0;
+
+        for (int k = 0; k < n; ++k) {
+            if (cnt.count(s[k + 1])) {
+                res += cnt[s[k + 1]] * k - total[s[k + 1]];
             }
+            cnt[s[k]]++;
+            total[s[k]] += k;
         }
         
         return res;
