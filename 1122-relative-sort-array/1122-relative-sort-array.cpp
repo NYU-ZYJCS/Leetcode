457a2ee4569d2379c00d43f16b598a8bd8484cbe
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-06-10 22:20:08 -0400

    Time: 0 ms (100.00%), Space: 9.4 MB (80.92%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int, int> freq;
+        int freq[1001];
         for (auto num : arr1) freq[num]++;
         
         int idx = 0;
@@ -14,10 +14,10 @@ public:
             }
         }
         
-        for (auto &[num, f] : freq) {
-            while (f > 0) {
-                arr1[idx++] = num;
-                freq[num]--;
+        for (int i = 0; i <= 1000; ++i) {
+            while (freq[i] > 0) {
+                arr1[idx++] = i;
+                freq[i]--;
             }
         }
         
