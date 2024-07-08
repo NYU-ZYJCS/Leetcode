51856b12a9638a77f1ba76ea8352242c02222f30
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-07-08 16:21:35 +0800

    Time: 2 ms (75.45%), Space: 7.1 MB (92.77%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        return circle(n, k) + 1;
+    }
+ 
+    
+private:
+    int circle(int n, int k) {
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            res = (res + k) % i;
+        }
+        return res;
+    }
+};
