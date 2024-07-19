918d6881ff9d0b5a5726b2eff0e1960b537fbc33
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-07-19 21:33:21 +0800

    Time: 16 ms (56.65%), Space: 14.4 MB (16.09%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -4,16 +4,16 @@ public:
         int m = matrix.size(), n = matrix[0].size();
 
         vector<int> minRow(m, INT_MAX), maxCol(n);
-        for(int i = 0 ; i < m ; i++){
+        for (int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 minRow[i] = min(minRow[i], matrix[i][j]);
                 maxCol[j] = max(maxCol[j], matrix[i][j]);
             }
         }
         vector<int> ans;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(minRow[i] == maxCol[j]) ans.push_back(matrix[i][j]);
+        for (int i = 0 ; i < m ; i++){
+            for (int j = 0 ; j < n ; j++){
+                if (minRow[i] == maxCol[j]) ans.push_back(matrix[i][j]);
             }
         }
         return ans;
