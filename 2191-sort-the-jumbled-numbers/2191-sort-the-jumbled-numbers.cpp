1ad45450ea6235aa08ff79c172d8302c37a0face
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-07-24 17:06:02 +0800

    Time: 473 ms (28.14%), Space: 76.5 MB (96.97%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        
+        function<int(int)> _transform = [&] (int num) {
+            if (num == 0) return mapping[0];
+            int ans = 0;
+            int multiply = 1;
+            while (num > 0) {
+                ans += multiply * mapping[num % 10];
+                num /= 10;
+                multiply *= 10;
+            }
+            
+            return ans;
+        };
+        
+        // increasing
+        ranges::sort(nums, [&] (const int& a, const int& b) {
+            return _transform(a) < _transform(b);
+        });
+        
+        return nums;
+    }
+    
+};
