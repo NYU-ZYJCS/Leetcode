40ee2397145a62efe8db56a6a19e84da30a3d0cb
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-08-03 04:43:27 -0700

    Time: 4 ms (92.55%), Space: 17.8 MB (33.93%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        vector<int> v(1001, 0);
+        for(int i = 0; i < target.size(); i++) {
+            v[target[i]]++;
+            v[arr[i]]--;
+        }
+        for(int i = 1; i <= 1000; i++) {
+            if(v[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
