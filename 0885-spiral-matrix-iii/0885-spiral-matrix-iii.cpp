d9eb9dd7f141624d308fc82bc298a58d8797ec32
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-08-08 14:16:41 -0700

    Time: 11 ms (42.36%), Space: 12.5 MB (92.73%) - LeetHub

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int n = rows * cols;
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        vector<vector<int>> res;
+        res.reserve(n);
+        res.push_back({rStart, cStart});
+        
+        int steps = 1, d = 0;
+        int r = rStart, c = cStart;
+        
+        while (true) {
+            for (int i = 0; i < steps; ++i) {
+                r += dir[d][0];
+                c += dir[d][1];
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    res.push_back({r, c});
+                }
+            }
+            
+            if (res.size() == n) break;
+            
+            d = (d + 1) % 4;
+            if (d % 2 == 0) steps++;
+        }
+        
+        return res;
+    }
+};
