d3a72b6712a7db345317dcf8093e503f5bdb9659
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-08-13 21:21:53 -0700

    Time: 12 ms (47.74%), Space: 12.6 MB (89.70%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        ranges::sort(nums);
+        
+        auto check = [&] (int target) -> bool {
+            int n = nums.size(), ans = 0;
+            for (int i = 0, j = 1; i < n; i++) {
+                while (j < n && nums[j] - nums[i] <= target) j++;
+                ans += j - i - 1;
+            }
+            return ans >= k;
+        };
+        
+        int l = 0, r = 1e6;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (check(mid)) r = mid;
+            else l = mid + 1;
+        }
+
+        return l;
+    }
+    
+    
+};
