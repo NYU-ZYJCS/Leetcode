64bb1ed8da1acd57d7010d7dcad765beca4442b5
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-08-17 08:56:08 -0700

    Time: 240 ms (36.79%), Space: 107.9 MB (57.08%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -5,7 +5,7 @@ public:
         int res = 0;
         int min_val = arrays[0][0], max_val = arrays[0].back();
         for (int i = 1; i < n; i ++) {
-            res = max(res, max( abs(max_val - arrays[i][0]), abs(arrays[i].back()-min_val) ) );
+            res = max(res, max(abs(max_val - arrays[i][0]), abs(arrays[i].back() - min_val)));
             min_val = min(min_val, arrays[i][0]);
             max_val = max(max_val, arrays[i].back());
         }
