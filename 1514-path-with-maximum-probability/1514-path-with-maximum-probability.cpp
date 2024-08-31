2dae28c12624861a2843e24a1caef1587b2cf197
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-08-31 11:06:22 -0400

    Time: 138 ms (26.85%), Space: 70.4 MB (39.92%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<double, int>>> graph(n);
+        vector<vector<pair<double, int>>> g(n);
         for (int i = 0; i < edges.size(); i++) {
             auto& e = edges[i];
-            graph[e[0]].emplace_back(succProb[i], e[1]);
-            graph[e[1]].emplace_back(succProb[i], e[0]);
+            g[e[0]].emplace_back(succProb[i], e[1]);
+            g[e[1]].emplace_back(succProb[i], e[0]);
         }
 
-        priority_queue<pair<double, int>> que;
+        priority_queue<pair<double, int>> q;
         vector<double> prob(n, 0);
-
-        que.emplace(1, start);
-        prob[start] = 1;
-        while (!que.empty()) {
-            auto [pr, node] = que.top();
-            que.pop();
-            if (pr < prob[node]) {
-                continue;
-            }
-            for (auto& [prNext, nodeNext] : graph[node]) {
-                if (prob[nodeNext] < prob[node] * prNext) {
-                    prob[nodeNext] = prob[node] * prNext;
-                    que.emplace(prob[nodeNext], nodeNext);
+        q.emplace(1.0, start);
+        prob[start] = 1.0;
+        
+        while (!q.empty()) {
+            auto [pb, node] = q.top(); 
+            q.pop();
+            
+            if (pb < prob[node]) continue;
+            
+            for (auto& [pbNext, nodeNext] : g[node]) {
+                if (prob[nodeNext] < prob[node] * pbNext) {
+                    prob[nodeNext] = prob[node] * pbNext;
+                    q.emplace(prob[nodeNext], nodeNext);
                 }
             }
         }
+        
         return prob[end];
     }
 };
