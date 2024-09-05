3ba8620103f10a6e5308eb45b99dae4ef36ef5eb
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-05 15:41:09 -0400

    Time: 0 ms (100.00%), Space: 7.3 MB (99.27%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -3,9 +3,8 @@ public:
     int minSteps(int n) {
         if (n == 1) return 0;
         int res = 0;
-        // i * j = n
         
-        for (int i = 2; i <= sqrt(n) && i < n; ++i) {
+        for (int i = 2; i <= sqrt(n); ++i) {
             while (n % i == 0) {
                 n /= i;
                 res += i;
