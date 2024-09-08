93beaa17b6399d20956524da623f0336a989ec0c
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-08 12:36:59 -0400

    Time: 110 ms (59.52%), Space: 44 MB (66.67%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.cpp
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -8,15 +8,15 @@ public:
         while (index1 < slots1.size() && index2 < slots2.size()) {
             int max_start = max(slots1[index1][0], slots2[index2][0]);
             int min_end = min(slots1[index1][1], slots2[index2][1]);
-            bool intersection = max_start <= min_end;
             int range = min_end - max_start;
             
-            if (intersection && range >= duration) {
+            if (range >= duration) {
                 return {max_start, max_start + duration};
-            } else {
-                if (slots1[index1][1] > slots2[index2][1]) index2++;
-                else index1++;
             }
+            
+            if (slots1[index1][1] > slots2[index2][1]) index2++;
+            else index1++;
+            
         }
         
         return {};
