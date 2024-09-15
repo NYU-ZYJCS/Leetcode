27fc8eb60cd8ed6a0e0b0e14958df65670bbb02e
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-15 12:09:47 -0400

    Time: 42 ms (73.26%), Space: 17.6 MB (85.12%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        int n = s.size();
+        vector<int> pos(32, INT_MAX);
+        pos[0] = -1; // Initialize pos[0] to -1
+        int status = 0, res = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            switch(c) {
+                case 'a': status ^= 1; break;
+                case 'e': status ^= 2; break;
+                case 'i': status ^= 4; break;
+                case 'o': status ^= 8; break;
+                case 'u': status ^= 16; break;
+                default: break;
+            }
+            if (pos[status] == INT_MAX) {
+                pos[status] = i;
+            } else {
+                res = max(res, i - pos[status]);
+            }
+        }
+        return res;
+    }
+
+};
