79b53d58f07761a758f48b50e1d6e7425777b750
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-20 15:11:46 -0400

    Time: 188 ms (25.74%), Space: 112.8 MB (37.92%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,45 @@
+class RandomizedCollection {
+public:
+    RandomizedCollection() {
+        
+    }
+    
+    bool insert(int val) {
+        nums.push_back(val);
+        mp[val].insert(nums.size() - 1);
+        return mp[val].size() == 1;
+    }
+    
+    bool remove(int val) {
+        if (mp.find(val) == mp.end()) return false;
+
+        int lastVal = nums.back();
+        int index = *mp[val].begin();
+        mp[val].erase(index);
+
+        nums[index] = lastVal;
+        mp[lastVal].insert(index);
+        mp[lastVal].erase(nums.size() - 1);
+        nums.pop_back();
+
+        
+        if (mp[val].size() == 0) mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+    
+private:
+    vector<int> nums;
+    unordered_map<int, unordered_set<int>> mp;
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
