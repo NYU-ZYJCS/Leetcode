81675fcec38e3bd1bf23a6c34524efa71fb7765c
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-20 22:58:28 -0400

    Time: 3 ms (94.22%), Space: 11.8 MB (98.65%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res(n);
+        
+        for (int i = 0, j = 1; i < n; ++i) {
+            res[i] = j;
+            if (j * 10 <= n) {
+                j *= 10;
+            } else {
+                while (j % 10 == 9 || j + 1 > n) j /= 10;
+                ++j;
+            }
+        }
+        
+        return res;
+    }
+};
