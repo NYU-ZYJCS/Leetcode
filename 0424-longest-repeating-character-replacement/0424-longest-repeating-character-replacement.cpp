f7b82c23efdcfa3c75e28718048e807ef6258880
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-24 16:41:54 -0400

    Time: 3 ms (96.57%), Space: 8.3 MB (99.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        int n = s.size();
         vector<int> freq(26);
         int max_count = 0, res = 0;
         
-        for (int left = 0, right = 0; right < s.size(); ++right) {
+        for (int left = 0, right = 0; right < n; ++right) {
             char c = s[right];
             max_count = max(max_count, ++freq[c - 'A']);
             
             if (right - left + 1 > max_count + k) {
                 --freq[s[left++] - 'A'];
+                continue;
             }
             
             res = max(res, right - left + 1);
         }
-        
         return res;
     }
 };
