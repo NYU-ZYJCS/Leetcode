18e3d74153300fcfe0b7b9cc9c7ec9e95a59a9c9
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-09-29 16:55:43 -0400

    Time: 101 ms (45.21%), Space: 62.1 MB (23.65%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,79 @@
+struct Node{
+    unordered_set<string> container;
+    int val = 0;
+    Node(int v) : val(v) {}
+};
+
+class AllOne {
+public:
+    unordered_map<string, list<Node>::iterator> kv;
+    list<Node> List;
+    
+    AllOne() {}
+    
+    void inc(string key) {
+        if (kv.contains(key)) {
+            auto oldNode = kv[key];
+            auto newNode = next(oldNode, 1);
+            
+            if (newNode == List.end() || newNode->val > oldNode->val + 1) {
+                newNode = List.insert(newNode, Node(oldNode->val + 1));
+            }
+
+            newNode->container.insert(key);
+            oldNode->container.erase(key);
+            
+            if (oldNode->container.empty()) {
+                List.erase(oldNode);
+            }
+            kv[key] = newNode;
+        } else {
+            auto newNode = List.begin();
+            
+            if (List.empty() || List.begin()->val > 1) {
+                newNode = List.insert(newNode, Node(1));
+            }
+            newNode->container.insert(key);
+            kv[key] = newNode;
+        }
+    }
+    
+    void dec(string key) {
+        if (kv.contains(key)){
+            auto oldNode = kv[key];
+            if (oldNode->val == 1) {
+                kv.erase(key);
+            } else {
+                auto newNode = next(oldNode, -1);
+                if (oldNode == List.begin() || newNode->val < oldNode->val - 1) {
+                    newNode = List.insert(oldNode, Node(oldNode->val - 1));
+                }
+                
+                newNode->container.insert(key);
+                kv[key] = newNode;
+            }
+            
+            oldNode->container.erase(key);
+            if (oldNode->container.empty()) {
+                List.erase(oldNode);
+            }
+        }
+    }
+    
+    string getMaxKey() {
+        return List.empty() ? "" : *List.rbegin()->container.begin();
+    }
+    
+    string getMinKey() {
+        return List.empty() ? "" : *List.begin()->container.begin();
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
