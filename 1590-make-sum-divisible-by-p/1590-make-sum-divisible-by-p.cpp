0032d4c0a8d4d75acf9cebae53985b00a3871c34
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-02 21:11:52 -0400

    Time: 143 ms (34.70%), Space: 70.5 MB (73.78%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -5,15 +5,17 @@ public:
         for (auto num : nums) x = (x + num) % p;
         
         if (x == 0) return 0;
-        unordered_map<int, int> idxs;
+        unordered_map<int, int> idx;
         int y = 0, res = nums.size();
+        idx[0] = -1;
         
         for (int i = 0; i < nums.size(); ++i) {
-            idxs[y] = i;
             y = (y + nums[i]) % p;
-            if (idxs.contains((y - x + p) % p)) {
-                res = min(res, i - idxs[(y - x + p) % p] + 1);
+            int pre = (y - x + p) % p;
+            if (idx.contains(pre)) {
+                res = min(res, i - idx[pre]);
             }
+            idx[y] = i;
         }
         
         return res == nums.size() ? -1 : res;
