15bf8639e3559dd823035bc5605c41df6a56c1ed
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-05 22:31:14 -0400

    Time: 5 ms (14.12%), Space: 9.2 MB (23.34%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        unordered_map<char, int> freq;
+        unordered_set<char> seen;
+        
+        for (auto c : s) ++freq[c];
+        
+        string res;
+        for (auto c : s) {
+            --freq[c];
+            if (seen.contains(c)) continue;
+            while (res.size() > 0 && c < res.back() && freq[res.back()]) {
+                seen.erase(res.back());
+                res.pop_back();
+            }
+            seen.insert(c);
+            res.push_back(c);
+        }
+        
+        return res;
+    }
+};
