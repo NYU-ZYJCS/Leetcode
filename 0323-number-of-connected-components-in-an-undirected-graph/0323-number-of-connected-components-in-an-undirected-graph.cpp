fa472155ea9422845e59b092d032068f9218a0a6
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-09 19:32:56 -0400

    Time: 13 ms (75.56%), Space: 16.7 MB (64.23%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    vector<int> parent;
+    vector<int> ranks;
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void _union(int x, int y) {
+        int px = find(x), py = find(y);
+        if (px == py) return;
+
+        if (ranks[px] < ranks[py]) {
+            parent[py] = px; 
+        } else if (ranks[px] > ranks[py]) {
+            parent[px] = py;
+        } else {
+            parent[px] = py;
+            ++ranks[py];
+        }
+    }
+
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        parent.resize(n);
+        ranks.resize(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+
+        for (auto e : edges) {
+            _union(e[0], e[1]);
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i == parent[i]) ++res;
+        }
+        return res;
+    }
+
+};
