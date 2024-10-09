f91171ff2046b73eb57434b440ef4a30b4453a53
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-08 21:38:53 -0400

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int left = 0, right = 0;
+        for (auto c : s) {
+            if (c == '(') ++left;
+            else {
+                if (left > 0) --left;
+                else ++right;
+            }
+        }
+        
+        return left + right;
+    }
+};
