13c36c80e0276d76cc4b3d1539ff67e074a7c968
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-20 09:40:01 -0400

    Time: 0 ms (100.00%), Space: 12.1 MB (82.16%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,42 +1,33 @@
 class Solution {
+    void dfs(vector<vector<int>> &grid, bool &closed, int x, int y) {
+        if (x == 0 || x == grid.size() - 1 || y == 0 || y == grid[x].size() - 1) {
+            if (grid[x][y] == 0) closed = false;
+            return;
+        }
+        
+        if (grid[x][y]) return;
+        
+        grid[x][y] = 1;
+        dfs(grid, closed, x - 1, y);
+        dfs(grid, closed, x + 1, y);
+        dfs(grid, closed, x, y - 1);
+        dfs(grid, closed, x, y + 1);
+    }
+
 public:
-    int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+    int closedIsland(vector<vector<int>> &grid) {
+        int m = grid.size(), n = grid[0].size(), ans = 0;
+        if (m < 3 || n < 3) return 0;
         
-        // traverse 0th row, (m-1)th row
-        for (int j = 0; j < n; ++j) {
-            dfs(grid, 0, j);
-            dfs(grid, m - 1, j);
-        }
-        
-        for (int i = 0; i < m; ++i) {
-            dfs(grid, i, 0);
-            dfs(grid, i, n - 1);
-        }
-        
-        int res = 0;
-        for (int i = 1; i < m - 1; ++i) {
-            for (int j = 1; j < n - 1; ++j) {
-                if (grid[i][j] == 0) {
-                    ++res;
-                    dfs(grid, i, j);
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (grid[i][j] == 0) { 
+                    bool closed = true;
+                    dfs(grid, closed, i, j);
+                    ans += closed;
                 }
             }
         }
-        
-        return res;
+        return ans;
     }
-    
-    void dfs(vector<vector<int>>& grid, int r, int c) {
-        int m = grid.size(), n = grid[0].size();
-        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 1) return;
-        
-        grid[r][c] = 1;
-        for (auto dir : directions) {
-            dfs(grid, r + dir[0], c + dir[1]);
-        }
-    }
-    
-private:
-    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 };
