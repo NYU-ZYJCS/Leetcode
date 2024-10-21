f2609801b05f15806c5ed4aa0172f27b793dbef6
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-21 10:49:09 -0400

    Time: 177 ms (53.17%), Space: 13.1 MB (77.57%) - LeetHub

---
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minStickers(vector<string>& stickers, string target) {
+        int n = target.size();
+        int mask = 1 << n;
+        
+        vector<int> dp(mask, INT_MAX);
+        dp[0] = 0;
+        
+        for (int i = 0; i < mask; ++i) {
+            if (dp[i] == INT_MAX) continue;
+            
+            for (string s : stickers) {
+                int cur = i, len = s.size();
+                
+                for (int k = 0; k < len; ++k) {
+                    char c = s[k];
+                    
+                    for (int p = 0; p < n; ++p) {
+                        if (target[p] == c && ((cur >> p) & 1) == 0) {
+                            cur |= (1 << p);
+                            break;
+                        }
+                    }
+                }
+                dp[cur] = min(dp[cur], dp[i] + 1);
+            }
+        }
+        
+        return dp[mask - 1] == INT_MAX ? -1 : dp[mask - 1];
+    }
+};
