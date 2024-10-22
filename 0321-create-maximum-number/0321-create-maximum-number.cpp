cb893b8b115128c20ddc8758224433ee6ee00ee9
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-22 13:54:39 -0400

    Time: 16 ms (85.04%), Space: 30.6 MB (66.87%) - LeetHub

--- 0321-create-maximum-number/0321-create-maximum-number.cpp
+++ 0321-create-maximum-number/0321-create-maximum-number.cpp
@@ -4,18 +4,18 @@ public:
         vector<int> res(k, 0);
         int m = nums1.size(), n = nums2.size();
         
-        // s length from s1, k - s length from s2
-        for (int s = max(0, k - n); s <= min(k, m); ++s) {
+        for (int len1 = max(0, k - n); len1 <= min(m, k); ++len1) {
             vector<int> temp;
             
-            vector<int> temp1 = maxKsequence(nums1, s);
-            vector<int> temp2 = maxKsequence(nums2, k - s);
+            vector<int> temp1 = maxKsequence(nums1, len1);
+            vector<int> temp2 = maxKsequence(nums2, k - len1);
             
             auto iter1 = temp1.begin(), iter2 = temp2.begin();
             while (iter1 != temp1.end() || iter2 != temp2.end()) {
                 temp.push_back(lexicographical_compare(iter1, temp1.end(), iter2, temp2.end()) ? *iter2++ : *iter1++);
             }
             res = lexicographical_compare(res.begin(), res.end(), temp.begin(), temp.end()) ? temp : res;
+            
         }
         
         return res;
@@ -23,20 +23,23 @@ public:
     
     vector<int> maxKsequence(vector<int>& nums, int k) {
         int n = nums.size();
+        
         if (n <= k) return nums;
         
-        vector<int> res;
+        vector<int> stk;
         int pop = n - k;
+        
         for (auto num : nums) {
             // decreasing
-            while (pop && res.size() > 0 && num > res.back()) {
-                res.pop_back();
-                --pop;
+            while (pop && stk.size() > 0 && num > stk.back()) {
+                stk.pop_back();
+                pop--;
             }
-            res.push_back(num);
+            
+            stk.push_back(num);
         }
         
-        res.resize(k);
-        return res;
+        stk.resize(k);
+        return stk;
     }
 };
