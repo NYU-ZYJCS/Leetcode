9eb17400a1318e4e967958105171f3264dfe8ff2
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-29 19:53:30 -0400

    Time: 3 ms (71.19%), Space: 9.6 MB (77.08%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     bool parseBoolExpr(string expression) {
         stack<char> stk;
-        
         int n = expression.size();
+        
         for (auto c : expression) {
             if (c == ',') continue;
             else if (c != ')') stk.push(c);
             else {
                 int t = 0, f = 0;
-                while (stk.top() != '(') {
-                    stk.top() == 't' ? ++t : ++f;
+                while (!stk.empty() && stk.top() != '(') {
+                    stk.top() == 't' ? t++ : f++;
                     stk.pop();
                 }
+                
                 stk.pop();
                 char op = stk.top();
                 stk.pop();
@@ -30,6 +31,7 @@ public:
                 }
             }
         }
+        
         return stk.top() == 't';
     }
 };
