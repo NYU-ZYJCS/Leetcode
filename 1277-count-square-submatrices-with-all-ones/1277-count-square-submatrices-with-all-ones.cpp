5db0c1420406029bc7439db8cad1ce4809e88451
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-29 15:32:42 -0400

    Time: 3 ms (83.38%), Space: 29.1 MB (38.41%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -4,6 +4,7 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         vector<vector<int>> f(m, vector<int>(n));
         int ans = 0;
+        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (i == 0 || j == 0) {
