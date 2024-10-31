2bee15e4696fda35ee0cec5e8070a9abae2a24de
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-10-31 13:59:16 -0400

    Time: 4 ms (68.17%), Space: 15.2 MB (31.26%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -18,20 +18,20 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         unordered_map<Node*, Node*> mp;
-        Node* cur = head;
-
+        auto cur = head;
+        
         while (cur) {
             mp[cur] = new Node(cur->val);
             cur = cur->next;
         }
-
+        
         cur = head;
         while (cur) {
             mp[cur]->next = mp[cur->next];
             mp[cur]->random = mp[cur->random];
             cur = cur->next;
         }
-
+        
         return mp[head];
     }
 };
