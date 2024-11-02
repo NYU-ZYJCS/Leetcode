26675211a7751304459a3121980c7d50c1e2dd8e
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-11-02 12:38:51 -0400

    Time: 331 ms (67.21%), Space: 236.9 MB (62.74%) - LeetHub

--- 1778-shortest-path-in-a-hidden-grid/1778-shortest-path-in-a-hidden-grid.cpp
+++ 1778-shortest-path-in-a-hidden-grid/1778-shortest-path-in-a-hidden-grid.cpp
@@ -11,45 +11,51 @@
 
 class Solution {
 public:
-    
-int g[2000][2000] = {};
-vector<pair<char, array<int, 2>>> dirs{{'U', {0, 1}}, {'D', {0, -1}}, {'L', {-1, 0}}, {'R', {1, 0}}};
+    int findShortestPath(GridMaster &master) {
+        int x = 1000, y = 1000, cnt = 0;
+        map(master, x, y, 'U');
 
-void map(GridMaster &master, int x, int y, char back) {
-    g[x][y] = master.isTarget() ? 2 : 1;
-    for (auto i = 0; i < dirs.size(); ++i) {
-        auto& [d, dxy] = dirs[i];
-        if (g[x + dxy[0]][y + dxy[1]] == 0) {
-            if (master.canMove(d)) {
-                master.move(d);
-                map(master, x + dxy[0], y + dxy[1], dirs[i % 2 ? i - 1 : i + 1].first);
+
+        vector<pair<int, int>> q{{x, y}};
+        while (!q.empty()) {
+            vector<pair<int, int>> q1;
+            for (auto [x, y] : q) {
+                if (g[x][y] == 2)
+                    return cnt;
+                if (g[x][y] == 1) {
+                    g[x][y] = -1;
+                    for (auto [_, dd] : dirs)
+                        q1.push_back({x + dd[0], y + dd[1]});
+                }
             }
-            else 
-                g[x + dxy[0]][y + dxy[1]] = -1;
+            ++cnt;
+            q = q1;
         }
+        
+        
+        return -1;
     }
-    master.move(back);
-}
-int findShortestPath(GridMaster &master) {
-    int x = 1000, y = 1000, cnt = 0;
-    map(master, x, y, 'U');
     
-    
-    vector<pair<int, int>> q{{x, y}};
-    while (!q.empty()) {
-        vector<pair<int, int>> q1;
-        for (auto [x, y] : q) {
-            if (g[x][y] == 2)
-                return cnt;
-            if (g[x][y] == 1) {
-                g[x][y] = -1;
-                for (auto [_, dd] : dirs)
-                    q1.push_back({x + dd[0], y + dd[1]});
+    void map(GridMaster &master, int x, int y, char back) {
+        g[x][y] = master.isTarget() ? 2 : 1;
+        
+        for (int i = 0; i < 4; ++i) {
+            auto& [d, dxy] = dirs[i];
+            int nx = x + dxy[0], ny = y + dxy[1];
+            
+            if (g[nx][ny] == 0) {
+                if (master.canMove(d)) {
+                    master.move(d);
+                    map(master, nx, ny, dirs[i % 2 ? i - 1 : i + 1].first);
+                } else {
+                    g[nx][ny] = -1;
+                }
             }
         }
-        ++cnt;
-        q = q1;
+        master.move(back);
     }
-    return -1;
-}
+    
+private:
+    int g[2000][2000] = {};
+    vector<pair<char, array<int, 2>>> dirs{{'U', {0, 1}}, {'D', {0, -1}}, {'L', {-1, 0}}, {'R', {1, 0}}};
 };
