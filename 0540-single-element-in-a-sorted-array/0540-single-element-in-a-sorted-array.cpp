7525c74a81751a8e5174ef5c23789f43fcab5840
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-11-18 21:34:12 -0500

    Time: 0 ms (100.00%), Space: 25 MB (63.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        // 0 1 2 3
+        // 1 1 2 2
+        
+        // 0 1 2 3
+        // 1 2 2 3
+        
+        // nums[i] == i / 2 + 1;
+        
         int n = nums.size();
         int left = 0, right = n - 1;
         
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (mid & 1) {
                 if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) left = mid + 1;
-                else right = mid;
+                else right = mid - 1;
             } else {
                 if (mid + 1 < n && nums[mid] == nums[mid + 1]) left = mid + 2;
-                else right = mid;
+                else right = mid - 1;
             }
         }
         
