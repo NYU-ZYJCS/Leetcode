4a2f10cc448c64e30bb920f3de9be7fba986f244
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-11-20 12:46:24 -0500

    Time: 4 ms (98.14%), Space: 87.9 MB (97.46%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int res = INT_MAX;
         int n = nums.size();
+        int res = INT_MAX;
         
         for (int i = 0; i < n; ++i) {
             if (nums[i] >= k) return 1;
-            
             for (int j = i - 1; j >= 0 && (nums[j] | nums[i]) != nums[j]; --j) {
                 nums[j] |= nums[i];
                 if (nums[j] >= k) {
