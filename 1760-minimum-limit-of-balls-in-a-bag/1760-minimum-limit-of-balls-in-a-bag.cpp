b74d57474dd326469d0338d011fae65880ef5c44
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-12-07 21:43:31 -0500

    Time: 31 ms (73.46%), Space: 59.7 MB (22.57%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -3,20 +3,20 @@ public:
     int minimumSize(vector<int>& nums, int maxOperations) {
         int left = 1, right = *max_element(nums.begin(), nums.end());
         int ans = 0;
+
         while (left <= right) {
-            int y = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             long long ops = 0;
-            for (int x: nums) {
-                ops += (x - 1) / y;
-            }
+            for (int x : nums) ops += (x - 1) / mid;
+            
             if (ops <= maxOperations) {
-                ans = y;
-                right = y - 1;
-            }
-            else {
-                left = y + 1;
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+        
         return ans;
     }
 };
