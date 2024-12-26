5c93becc2f875982be5f7962f921ac203ac4c370
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-12-25 23:37:11 -0500

    Time: 0 ms (100.00%), Space: 12.4 MB (62.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        // x + y = target
-        // x + abs(y) = sum
-        
-        // x = target + sum >> 1;
-        
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum < abs(target) || ((sum + target) & 1)) return 0;
         
-        int t = sum + target >> 1;
+        int t = (sum + target) >> 1;
         vector<int> dp(t + 1, 0);
         
         dp[0] = 1;
         for (int num : nums) {
-            for (int j = t; j >= num; --j) {
-                dp[j] += dp[j - num];
+            for (int i = t; i >= num; --i) {
+                dp[i] += dp[i - num];
             }
         }
-        
         return dp.back();
     }
 };
