7974c279c8f1f9496287d126891178b0f564d019
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-12-25 20:05:39 -0500

    Time: 113 ms (22.08%), Space: 62.3 MB (26.00%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -10,21 +10,32 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* node = lca(root, p, q);
+        TreeNode* node = helper(root, p, q);
         if (p_exist && q_exist) return node;
         return nullptr;
     }
     
-    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (root == nullptr) return root;
         
-        TreeNode* left = lca(root->left, p, q);
-        TreeNode* right = lca(root->right, p, q);
+//         if (root == p) {
+//             p_exist = true;
+//             return root;
+//         }
+        
+//         if (root == q) {
+//             q_exist = true;
+//             return root;
+//         }
+        
+        TreeNode* left = helper(root->left, p, q);
+        TreeNode* right = helper(root->right, p, q);
         
         if (root == p) {
             p_exist = true;
             return root;
         }
+        
         if (root == q) {
             q_exist = true;
             return root;
