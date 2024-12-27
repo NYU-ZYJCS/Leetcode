9b7af51ac38d7992caa3c425de540575f7c809b9
Yingjie Zhang <149397958+NYU-ZYJCS@users.noreply.github.com>
2024-12-26 22:09:07 -0500

    Time: 0 ms (100.00%), Space: 45.6 MB (55.58%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int res = 0, mx = values[0] + 0;
+        
+        for (int i = 1; i < values.size(); ++i) {
+            res = max(res, mx + values[i] - i);
+            mx = max(mx, values[i] + i);
+        }
+        
+        return res;
+    }
+};
